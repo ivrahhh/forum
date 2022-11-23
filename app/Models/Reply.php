4179867db7d1e1b7fd6f84837150f0bbb29fcf32
harvicapino22 <harvicapino22@gmail.com>
2022-11-23 18:00:25 +0800

    added primary models

---
+++ app/Models/Reply.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reply extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'reply',
+        'comment_id',
+    ];
+
+    public function comment() : BelongsTo
+    {
+        return $this->belongsTo(Comment::class,'comment_id');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => Registered::class,
+    ];
+
+    public function posts() : HasMany
+    {
+        return $this->hasMany(Post::class,'user_id');
+    }
+
+    public function comments() : HasMany
+    {
+        return $this->hasMany(Comment::class,'user_id');
+    }
+
+    public function replies() : HasMany
+    {
+        return $this->hasMany(Reply::class,'user_id');
+    }
+}
